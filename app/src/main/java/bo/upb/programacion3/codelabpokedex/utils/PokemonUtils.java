b2ebefc53a67ac83d519ff920864f9746fffc98f
manuflores <flores.morales.manuela@gmail.com>
2020-06-18 00:27:10 -0400

    Tarea terminada :D

--- app/src/main/java/bo/upb/programacion3/codelabpokedex/utils/PokemonUtils.java
+++ app/src/main/java/bo/upb/programacion3/codelabpokedex/utils/PokemonUtils.java
@@ -25,7 +25,16 @@ public class PokemonUtils {
         pokemons.add(new Pokemon(9, "Rattata", R.drawable.rattata, "Normal"));
         pokemons.add(new Pokemon(10, "Sandshrew", R.drawable.sandshrew, "Ground"));
         pokemons.add(new Pokemon(11, "Venonat", R.drawable.venonat, "Bug"));
-        pokemons.add(new Pokemon(12, "Zubat", R.drawable.zubat, "Poison"));
+        pokemons.add(new Pokemon(12, "Ban", R.drawable.ban, "Inmortal"));
+        pokemons.add(new Pokemon(13, "Gowther", R.drawable.gowther, "Wizard"));
+        pokemons.add(new Pokemon(14, "Jericho", R.drawable.jericho, "Knight"));
+        pokemons.add(new Pokemon(15, "Scannor", R.drawable.scannor, "Human"));
+        pokemons.add(new Pokemon(16, "Diane", R.drawable.diane, "Gigant"));
+        pokemons.add(new Pokemon(17, "Hawk", R.drawable.hawk, "Caos"));
+        pokemons.add(new Pokemon(18, "Elizabeth", R.drawable.elizabeth, "Goddess"));
+        pokemons.add(new Pokemon(19, "King", R.drawable.king, "Fairy"));
+        pokemons.add(new Pokemon(20, "Merlin", R.drawable.merlin, "Wizard"));
+        pokemons.add(new Pokemon(21, "Meliodas", R.drawable.meliodas, "Demon"));
         return pokemons;
     }
 }
